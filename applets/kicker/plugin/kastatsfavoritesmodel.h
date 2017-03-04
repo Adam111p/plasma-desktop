3e4a27def3d05ea8379fc34d661243e037b8b398
Ivan Čukić <ivan.cukic@kde.org>
2017-03-04 22:30:14 +0100

    Fixed licensing

--- applets/kicker/plugin/kastatsfavoritesmodel.h
+++ applets/kicker/plugin/kastatsfavoritesmodel.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2014-2015 by Eike Hein <hein@kde.org>                   *
- *   Copyright (C) 2016-2015 by Ivan Cukic <ivan.cukic@kde.org>            *
+ *   Copyright (C) 2016-2017 by Ivan Cukic <ivan.cukic@kde.org>            *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
--- applets/kicker/plugin/placeholdermodel.h
+++ applets/kicker/plugin/placeholdermodel.h
@@ -1,5 +1,6 @@
 /***************************************************************************
  *   Copyright (C) 2015 by Eike Hein <hein@kde.org>                        *
+ *   Copyright (C) 2017 by Ivan Cukic <ivan.cukic@kde.org>                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
